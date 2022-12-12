2ab9971cd0e40cca780eb4e5547cb31d86c6ee84
KaraJH <karas.jones@yahoo.com>
2022-12-12 16:56:47 -0700

    test: Add player method test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Team do
     team = Team.new("France")
 
     expect(team.country).to eq("France")
+    expect(team.players).to eq([])
   end
 
   it 'starts off as not eliminated, and can be eliminated' do 
@@ -15,5 +16,22 @@ RSpec.describe Team do
     team.eliminated
 
     expect(team.eliminated?).to eq(true)
+  end
+
+  describe '#add_player' do 
+    it 'can add players to the team' do 
+      team = Team.new("France")
+
+      mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+      pogba = Player.new({name: "Paul Pogba", position: "midfielder"}) 
+
+      expect(team.players).to eq([])
+
+      team.add_player(mbappe) 
+      team.add_player(pogba)     
+
+      expect(team.players).to eq([mbappe, pogba])
+      
+    end
   end
 end
