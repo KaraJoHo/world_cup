d5d554b1f6e057bc165f8327574eececfa981ded
KaraJH <karas.jones@yahoo.com>
2022-12-12 18:08:05 -0700

    test: All players by position hash test

--- spec/world_cup_spec.rb
+++ spec/world_cup_spec.rb
@@ -54,4 +54,35 @@ RSpec.describe WorldCup do
       expect(world_cup.active_players_by_position("midfielder")).to eq([pogba])
     end
   end
+
+  describe '#all_players_by_position' do 
+    it 'lists players by position in a hash' do 
+      france = Team.new("France") 
+
+      mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})    
+      pogba = Player.new({name: "Paul Pogba", position: "midfielder"})  
+
+      france.add_player(mbappe)    
+      france.add_player(pogba)    
+
+      croatia = Team.new("Croatia")  
+
+      modric = Player.new({name: "Luka Modric", position: "midfielder"})    
+      vida = Player.new({name: "Domagoj Vida", position: "defender"}) 
+
+      croatia.add_player(modric)    
+      croatia.add_player(vida)    
+
+      world_cup = WorldCup.new(2018, [france, croatia])  
+
+      expected = { 
+                    "forward" => [mbappe],
+                    "midfielder" => [pogba, modric],
+                    "defender" => [vida]
+      }
+
+      expect(world_cup.all_players_by_position).to eq(expected)
+      
+    end
+  end
 end
