5b9a6765231280c57512554ee8791d6fc05fff2a
KaraJH <karas.jones@yahoo.com>
2022-12-12 17:11:54 -0700

    test: Active players by position method test

--- spec/world_cup_spec.rb
+++ spec/world_cup_spec.rb
@@ -26,4 +26,31 @@ RSpec.describe WorldCup do
     expect(world_cup.year).to eq(2018)
     expect(world_cup.teams).to eq([france, croatia])
   end
+
+  describe '#active_players_by_position' do 
+    it 'is an active player, only when the team is not eliminated' do 
+      mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})    
+      pogba = Player.new({name: "Paul Pogba", position: "midfielder"})  
+
+      france.add_player(mbappe)    
+      france.add_player(pogba)    
+
+      croatia = Team.new("Croatia")  
+
+      modric = Player.new({name: "Luka Modric", position: "midfielder"})    
+      vida = Player.new({name: "Domagoj Vida", position: "defender"}) 
+
+      croatia.add_player(modric)    
+      croatia.add_player(vida)    
+
+      world_cup = WorldCup.new(2018, [france, croatia])  
+
+      expect(world_cup.active_players_by_position("midfielder")).to eq([pogba, modric])
+
+      croatia.eliminated 
+      
+      expect(world_cup.active_players_by_position("midfielder")).to eq([pogba])
+
+    end
+  end
 end
