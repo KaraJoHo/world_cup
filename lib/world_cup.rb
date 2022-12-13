c8cee9b07bef7563138aca0f718d410ac351b7a0
KaraJH <karas.jones@yahoo.com>
2022-12-12 17:52:57 -0700

    feat: active players by position added

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -5,4 +5,13 @@ class WorldCup
     @year = year 
     @teams = teams
   end
+
+  def active_players_by_position(position)
+    @teams.map do |team|
+      if team.elimination_status == false
+        team.players_by_position(position)
+      end
+    end.flatten.compact
+  end
+
 end
