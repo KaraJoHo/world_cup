7e68b08fd6c5ce330a256608c4a451357ec90deb
KaraJH <karas.jones@yahoo.com>
2022-12-12 18:03:43 -0700

    Trying other ideas for active players by position

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -14,4 +14,19 @@ class WorldCup
     end.flatten.compact
   end
 
+  # def active_players_by_position(position)
+  #   @teams.map do |team|
+  #     team.players_by_position(position).select do |player| 
+  #      team.elimination_status == false
+  #     end
+  #   end
+  # end
+  #trying to figure out a better waaayyy..
+
+  #@teams.select do |team| 
+  #team.elimination_status == false
+  #team.players_by_position(position)
+  #end
+  #just returns the whole team object.
+
 end
